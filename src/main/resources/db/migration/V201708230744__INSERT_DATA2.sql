4a892933e5d2cf8db79c4e8ceb1dbf4d52e25cfd
AliRabeeMahmoud <eng.alirabee@gmail.com>
2024-06-04 12:45:42 +0200

    start

---
+++ src/main/resources/db/migration/V201708230744__INSERT_DATA2.sql
@@ -0,0 +1,13 @@
+ insert 
+    into
+        employee_table
+        (email, employee_name, salary) 
+    values
+        ('ali.b@gmail.com', 'ali Bingel', 90000.00);
+        
+ insert 
+    into
+        employee_table
+        (email, employee_name, salary) 
+    values
+        ('rabee.b@gmail.com', 'rabee Bingel', 560000.00);
